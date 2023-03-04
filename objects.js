7c9c5fb415c0edee9846bc64a43e623297f28798
max31ru12 <maxevg72@gmail.com>
2023-03-04 18:45:04 +0300

    learned about objects

---
+++ objects.js
@@ -0,0 +1,87 @@
+// Пустой объект (два способа создания)
+let userOne = new Object();
+let userTwo = {};
+
+
+let userThree = {
+    name: "John",
+    age: 23
+};
+
+// Обращение к свойствам
+console.log(`Имя пользователя: ${userThree.name}, его возраст: ${userThree.age}`);
+
+// добавить свойство
+userThree.isAdmin = false;
+console.log(userThree.isAdmin);
+
+// Удалить свойство
+delete userThree.age;
+
+// Свойство из нескольких слов
+let userFour = {
+    name: "Max",
+    age: 21,
+    "user's description": 'Default user'
+};
+
+// обращение через квадратные скобки
+console.log(userFour["user's description"]);
+console.log(userFour["name"]);
+
+
+let fruit = "apple";
+
+let bag = {
+    // Имя свойства будет взято из переменной fruit
+    [fruit]: 5,
+}
+
+console.log(bag.apple);
+
+
+// Проверка существования свойства
+console.log(bag.orange === undefined); // true, потому точто св-ва не существует
+
+console.log("orange" in bag); // false, bag не имеет сво-ва orange
+
+
+// Перебор свойств объекта
+for (key in bag) {
+    console.log(key);
+    console.log(bag[key]);
+}
+
+
+// Копирование объектов
+
+// При копировании переменной объекта копируется ссылка, но сам объект не дублируется.
+let user = {name: "Jonh"};
+let admin = user; // Передаем ссылку на объект в переменную
+
+admin["age"] = 23;
+console.log(user.age); // В user теперь тоже есть св-во
+console.log(user === admin); // Обе перменные ссылаются на один и тот же объект, поэтому true
+
+let a = {}, b = {};
+console.log(a === b); // Это два независимых объекта, false
+
+
+// Создать такой же, но независимый объект (клон) - только в цикле for in (нет стандартного метода)
+let newUser = {};
+for (key in user) {
+    newUser[key] = user[key];
+}
+
+console.log(user);
+console.log(newUser);
+
+let newAdmin = {};
+// Метод копирует св-ва объектов user, admin и тд в объект newAdmin
+Object.assign(newAdmin, admin, user); //
+console.log(newAdmin);
+
+// Такое применение
+let clone = Object.assign({}, user);
+console.log(clone);
+
